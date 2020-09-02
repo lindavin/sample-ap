1081fa591e178f70069b0ce13e7f0187518b2258
Davin Lin <lindavin@grinnell.edu>
2020-09-02 23:54:41 +0000

    Made page title more modular

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    return (if(page_title.empty?) then base_title else page_title + ' | ' +  base_title end)
+  end
 end
