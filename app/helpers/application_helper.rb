472223eb1394130116aa4ff27d04e904c3286a37
Davin Lin <lindavin@grinnell.edu>
2020-09-03 00:32:52 +0000

    Refactor helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
+  # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = 'Ruby on Rails Tutorial Sample App'
-    return (if(page_title.empty?) then base_title else page_title + ' | ' +  base_title end)
+    "#{(page_title + ' | ') unless  page_title.empty?}Ruby on Rails Tutorial Sample App"
   end
 end
