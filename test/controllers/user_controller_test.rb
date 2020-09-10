11baaf038f239334b0cb43db15087bcd054c1ef0
Davin Lin <lindavin@grinnell.edu>
2020-09-10 00:53:20 +0000

    Add test for user signup page

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
-  end
 
   test "signup" do
     get signup_path
