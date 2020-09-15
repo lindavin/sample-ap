867c9c2b1c8adc90770bb2166a3f91fb92b8710d
Davin Lin <lindavin@grinnell.edu>
2020-09-15 23:57:39 +0000

    Test User model validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Hello", email: "Human")
+  end 
+  
+  test "Should be valid" do
+   assert @user.valid?
+  end
+  
 end
