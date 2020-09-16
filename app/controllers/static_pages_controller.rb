a92331316f9fecabb22ecdef32003ea2359d378b
Davin Lin <lindavin@grinnell.edu>
2020-09-16 01:02:37 +0000

    Add static rss feed from mint press

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
-    puts('hello')
+      # code-snippet from: https://medium.com/@josheche/parsing-an-rss-feed-in-ruby-on-rails-58b23cfb5b25
+      require 'rss'
+      require 'open-uri'
+      url = 'https://www.mintpressnews.com/rss/'
+      rss = RSS::Parser.parse(open(url).read, false).items[0..5]
+      @rss_results = []
+      rss.each do |result|
+      result = { title: result.title, date: result.pubDate, link: result.link, description: result.description }
+      @rss_results.push(result)
+      end
   end
 
   def help
