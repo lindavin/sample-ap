15cb3f5da8e84b4027999fdb282272dcc5843741
Davin Lin <lindavin@grinnell.edu>
2020-09-16 19:47:25 -0400

    Update schema to have user.email be an index and have remove fixture tests according to Hartl

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_002022) do
+ActiveRecord::Schema.define(version: 2020_09_16_234417) do
+
+  create_table "rss_feeds", force: :cascade do |t|
+    t.string "name"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
