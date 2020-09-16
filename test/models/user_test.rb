4a85d20eece7953ac88185b27e50f2c724afde0e
Davin Lin <lindavin@grinnell.edu>
2020-09-16 19:40:17 -0400

    Fix uniqueness typo and add tests for uniquess

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,4 +22,9 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  test "User should be unique" do
+    duplicate_user = @user.dup 
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
