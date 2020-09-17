1801375374be28a5f7700264e67394780c2b3014
Davin Lin <lindavin@grinnell.edu>
2020-09-16 21:54:40 -0400

    HW7

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,36 @@
 class UserController < ApplicationController
+  # The line [protect_from_forgery prepend:true] immediately below was suggested at:
+  # https://stackoverflow.com/questions/38331496/rails-5-actioncontrollerinvalidauthenticitytoken-error
+  protect_from_forgery prepend:true
+  
   def new
+    @user = User.new
   end
   
   def login 
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   
-  resources :user
   resources :rss_feeds
   get '/login' , to:'user#login'
   get '/signup' , to:'user#new'
   get '/help' , to:'static_pages#help'
   get '/about', to:'static_pages#about'
   get '/contact', to:'static_pages#contact'
-
+  resources :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
